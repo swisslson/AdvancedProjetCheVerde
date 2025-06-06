b8033ad96840798d8ab0505874a5a7e5e8d2db9a
swisslson <cyril.hany@gmail.com>
2025-06-06 14:45:39 +0200

    ajout page profil dans main

--- HackingUrbain/Profil/ProfilView.swift
+++ HackingUrbain/Profil/ProfilView.swift
@@ -9,19 +9,129 @@ import SwiftUI
 
 struct ProfilView: View {
     
+    private let columns = Array(repeating: GridItem(.flexible()), count: 3)
+//    @State private var user = User
     
     var body: some View {
-        
-        Text("profilview")
-        
-        
-        
-        
+        VStack{
+            
+            VStack{
+                Image(user.photo)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 160, height: 160)
+                    .clipShape(Circle())
+//                              .shadow(radius: 5)
+                Text("Hey \(user.pseudo) !")
+                    .font(Font.custom("InstrumentSans-Bold", size: 30))
+                    .foregroundColor(Color.black)
+                // fair la fonts
+                    .padding(.top, 5)
+                
+            }// fin de vsteack photo speudo
+            .padding(.bottom, 15.0)
+            
+            
+            VStack{
+                Text("Descprition")
+                    .font(Font.custom("InstrumentSans-Bold", size: 14))
+
+                    .foregroundColor(Color.black)
+                    .multilineTextAlignment(.leading)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.bottom, 10.0)
+                
+                
+                // fair la fonts
+                VStack{
+                    Text(user.description)
+                        .font(.system(size: 12))
+                        .foregroundColor(Color.black)
+                        .multilineTextAlignment(.leading)
+                        .padding(.bottom, 10)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    // fair la fonts
+                }
+                
+                
+            }// fin de la stack description
+            //            .padding(.bottom, 300)
+            .padding(.horizontal, 30)
+            
+            
+            
+            
+            VStack {
+                Text("Mes Badges")
+                    .font(Font.custom("InstrumentSans-Bold", size: 14))
+                    .foregroundColor(Color.black)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                ScrollView(.horizontal) {
+                    HStack(spacing: 15) {
+                        ForEach(user.badges) { badge in
+                        VStack {
+                            Image(badge.imageName)
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 70, height: 70)
+                            Text(badge.name)
+                                .font(.system(size: 12))
+                                .foregroundColor(Color.black)
+                                .padding(.top, 3)
+                            
+                            
+                            }
+                        }
+                  
+                        
+                        
+                        
+                    }
+                    
+                }
+            }// fin de v stack badges
+            .padding(.horizontal, 30)
+            
+            
+            // boucle et verif condition is == true pour afficher struct person-> [logo = image == false/ image == true]
+            
+            
+            
+            // boucle affichage photo prise struc liste dimage
+            // Galerie
+            VStack{
+                Text("Ma galerie")
+                    .font(Font.custom("InstrumentSans-Bold", size: 14))
+                    .foregroundColor(Color.black)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading, 5)
+                    .padding([.bottom, .top], 10)
+                    
+                LazyVGrid(columns: columns, spacing: 10) {
+                    ForEach(user.gallery, id: \.self) { imageName in
+                        Image(imageName)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 100, height: 100)
+                            .clipped()
+                            .cornerRadius(20)
+                    }
+                }
+                .padding(.horizontal, 10.0)
+            }.padding(.horizontal, 30)
+               
+            
+            
+        }// fin de vstack de page
+    
         
     }// fin du body
     
     
 }// fin du ProfilView
+
+
+
 
 
 
--- HackingUrbain/Struct/Struct.swift
+++ HackingUrbain/Struct/Struct.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+import UIKit
+import SwiftUICore
+
+struct Badge: Identifiable {
+    let id = UUID()
+    let name: String
+    let isActive: Bool
+    let activeImage: String
+    let inactiveImage: String
+    let decsription: String
+    
+    var imageName: String {
+        isActive ? activeImage : inactiveImage
+    }
+}
+
+struct User {
+    var photo: String
+    var pseudo: String
+    var description: String
+    var badges: [Badge]
+    var gallery: [String]
+}
+
+  let user = User(
+            photo: "user4",
+            pseudo: "Alizé",
+            description: "Voici ma description d'utilisateur.Voici ma description d'utilisateur.Voici ma description d'utilisateur.",
+            badges: [
+                Badge(name: "débutant", isActive: true, activeImage: "perso1", inactiveImage: "perso-nb1",decsription: "a"),
+                Badge(name: "1er participation", isActive: false, activeImage: "perso2", inactiveImage: "perso-nb2",decsription: "a")
+            ],
+            gallery: ["albumCompte1",
+                      "albumCompte2",
+                      "albumCompte3",
+                      "albumCompte4",
+                      "albumCompte5",
+                      "albumCompte6"
+                
+            ]
+        )
